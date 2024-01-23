f1752e05d02eece235aabde2c432e7c023ffee58
Lina Parren <lina3@hotmail.nl>
2024-01-23 14:08:28 +0100

    Added comments

--- server.js
+++ server.js
@@ -2,38 +2,44 @@ import express  from 'express';
 import fs  from 'fs';
 import cors  from 'cors';
 
+// Create Express app
 const app = express();
 
-app.use(express.json());
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors())
+app.use(express.json());
 
 var corsOptions = {
+  // Allow requests from this origin
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200
 }
 
 app.get('/api', cors(corsOptions),(req, res) => {
-  console.log(req)
+  // Read the contents of 'api.json'
   const rawData = fs.readFileSync('api.json');
+  // Parse JSON data
   const data = JSON.parse(rawData);
+  // Respond with JSON data
   res.json(data);
 });
 
 app.post('/api', cors(corsOptions), (req, res) => {
+  // Get updated items from request
   const updatedItems = req.body.items;
+  // Read the contents of 'api.json'
   const rawData = fs.readFileSync('api.json');
+  // Parse JSON data
   const data = JSON.parse(rawData);
 
+  // Update items in api.json with new data
   data.items.forEach((item, index) => {
     item.completed = updatedItems[index].completed;
     item.name = updatedItems[index].name;
     item.date = updatedItems[index].date;
   });
+  // Change data in 'api.json' to updated data
   fs.writeFileSync('api.json', JSON.stringify(data, null, 2));
 
   res.json(data);
 });
-
-app.listen(3000, () => {
-    console.log(`server.js zegt: Example app listening on port ${3000}`)
-})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <!-- Popup that shows when bingo == true -->
     <div class="popup-bingo" v-if="bingo">
       <h2>bingo!!</h2>
       <p>time for a mario kart game night</p>
     </div>
+    <!-- Bingo card is created based on items in listAPI -->
     <div class="bingo-card">
-      <div v-for="(item, index) in apiData.items" :key="index" class="card-cell" @click="toggleComplete(item)">
+      <!-- Click on item runs markItem() -->
+      <div v-for="(item, index) in listAPI.items" :key="index" class="card-cell" @click="markItem(item)">
         <div :class="{ 'completed': item.completed }">
           <div class="startext">
             <div class="star"></div>
@@ -22,82 +25,102 @@
 export default {
   data() {
     return {
-      apiData: { items: [] },
+      listAPI: { items: [] },
       rows: 5,
       cols: 5,
       bingo: false
     };
   },
   mounted() {
+    // Get the data from api.json
     fetch('/api.json')
       .then(response => response.json())
       .then(data => {
-        this.apiData = data;
-        this.markNumber();
+        this.listAPI = data;
+        this.checkBingo();
       })
       .catch(error => console.error('Error fetching data:', error));
   },
   methods: {
-    toggleComplete(item) {
+    // Function that marks an item when clicked
+    markItem(item) {
+        // Do if item has not been marked yet
         if (!item.completed) {
+          // Ask user for name, this will be displayed on the marked item
           item.name = prompt("Please enter your name") + ", ";
+          // Get date at time of click, this will be displayed on the marked item
           item.date = new Date().toLocaleDateString([], { month: 'short', day: 'numeric' });
         } else {
+          // If item was clicked, switch back to not-clicked state
           item.name = "";
           item.date = ""
         }
+        // Change value for .completed in api.json
         item.completed = !item.completed;
+        // Run following functions
         this.updateAPI();
-        this.markNumber();
+        this.checkBingo();
     },
     updateAPI() {
-        // console.log(this.apiData.items);
+        // Save the changes to items in api.json
         fetch('http://localhost:3000/api', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ items: this.apiData.items }),
+            body: JSON.stringify({ items: this.listAPI.items }),
         })
         .then(response => response.json())
         .catch(error => console.error('Error:', error));
     },
-    markNumber() {
+    checkBingo() {
+      // Set bingo = false to start with
       this.bingo = false;
+      // Get keys that form columns
       for (let column = 0; column < this.cols; column++) {
+        // Set all columns and all rows to true
         let columnComplete = true;
         let rowComplete = true;
+        // Get keys that form rows
         for (let row = 0; row < this.rows; row++) {
+          // Each item in order to make columns, from left to right
           const vertical = column + (row * this.cols);
+          // Each item in order to make rows, from top to bottom
           const horizontal = row + (column * this.cols);
-          if (!this.apiData.items[vertical].completed) {
+          if (!this.listAPI.items[vertical].completed) {
+            // If an item in vertical is not completed, set columnComplete = false
             columnComplete = false;
           }
-          if (!this.apiData.items[horizontal].completed) {
+          if (!this.listAPI.items[horizontal].completed) {
+            // If an item in horizontal is not completed, set rowComplete = false
             rowComplete = false;
           }
+          // For each row, set bingo == true if rowComplete == true
           if (row == this.rows - 1) {
-            // console.log(`Row ${column + 1} complete?`, rowComplete);
             if (rowComplete) this.bingo = true;
           }
         }
-        // console.log(`Column ${column + 1} complete?`, columnComplete);
+        // For each column, set bingo == true if columnComplete == true
         if (columnComplete) this.bingo = true;
       }
 
-
+      // Make arrays to store keys for diagonalLeft and diagonalRight
       const diagonalLeft = [];
       const diagonalRight = [];
+      // Get keys that make diagonal from top left to bottom right
       for (let i = 0; i <= ((this.rows * this.cols) - 1); i += this.cols + 1) {
+        // Save found keys in array
         diagonalLeft.push(i);
       }
+      // Get keys that make diagonal from top right to bottom left
       for (let i = this.rows - 1; i < ((this.rows * this.cols) - 1); i += this.cols - 1) {
+        // Save found keys in array
         diagonalRight.push(i);
       }
-      const diagLeftComplete = diagonalLeft.every(i => this.apiData.items[i].completed);
-      const diagRightComplete = diagonalRight.every(i => this.apiData.items[i].completed);
-      // console.log('Diagonal from left complete?', diagLeftComplete);
-      // console.log('Diagonal from right complete?', diagRightComplete);
+      // Make variables if all keys in array are set to completed
+      const diagLeftComplete = diagonalLeft.every(i => this.listAPI.items[i].completed);
+      const diagRightComplete = diagonalRight.every(i => this.listAPI.items[i].completed);
+      // For diagLeftComplete and diagRightComplete, set bingo == true if either is true
       if (diagLeftComplete == true || diagRightComplete == true) {
         this.bingo = true;
       }
@@ -105,6 +128,13 @@ export default {
   }
 };
 </script>
+
+<!--
+console.log(`Row ${column + 1} complete?`, rowComplete);
+console.log(`Column ${column + 1} complete?`, columnComplete);
+console.log('Diagonal from left complete?', diagLeftComplete);
+console.log('Diagonal from right complete?', diagRightComplete);
+-->
 
 <!--
   Horizontal:
